a46bc73d14cf8f2cfec8a12ca7077952a3d4a9f7
y28lyn <i.beirade@gmail.com>
2023-11-14 11:49:34 +0100

    vercel test

---
+++ src/pages/MentionLegales.tsx
@@ -0,0 +1,80 @@
+const MentionLegales = () => {
+  return (
+    <>
+      <div className="bg-[#000302] text-white pb-10 pt-20 px-10 md:p-28">
+        <h1 className="text-3xl font-bold">Mentions Légales</h1>
+        <p className="text-sm">
+          <strong>Date de la Dernière Mise à Jour :</strong> 19/10/2023
+        </p>
+
+        <section className="mt-6">
+          <h2 className="text-xl font-bold">Responsable du Site Web</h2>
+          <p>Responsable éditorial : Ilyes BEIRADE</p>
+          <p>Adresse : Adresse</p>
+          <p>Téléphone : Téléphone</p>
+          <p>E-mail : E-Mail</p>
+        </section>
+
+        <section className="mt-6">
+          <h2 className="text-xl font-bold">Hébergeur du Site Web</h2>
+          <p>Nom de l'hébergeur : Nom</p>
+          <p>Siège social : Adresse</p>
+          <p>Téléphone : Numéro </p>
+        </section>
+
+        <section className="mt-6">
+          <h2 className="text-xl font-bold">
+            Aucune Collecte de Données Personnelles
+          </h2>
+          <p>
+            Nous n'effectuons aucune collecte de données personnelles sur notre
+            site web. Vous pouvez naviguer sur notre site en toute
+            confidentialité sans que vos informations personnelles ne soient
+            collectées ou stockées.
+          </p>
+        </section>
+
+        <section className="mt-6">
+          <h2 className="text-xl font-bold">Aucun Usage de Cookies</h2>
+          <p>
+            Notre site web n'utilise pas de cookies pour collecter des
+            informations sur les utilisateurs. Votre expérience de navigation
+            est entièrement libre de tout suivi via des cookies.
+          </p>
+        </section>
+
+        <section className="mt-6">
+          <h2 className="text-xl font-bold">
+            Utilisation de Composants de Tailbits
+          </h2>
+          <p>
+            Certaines parties de ce site web utilisent des composants de l'UI
+            kit "Celebration" créés par Tailbits. Ces composants sont utilisés
+            conformément à la licence applicable de Tailbits.
+          </p>
+        </section>
+
+        <section className="mt-6">
+          <h2 className="text-xl font-bold">Propriété Intellectuelle</h2>
+          <p>
+            Tous les contenus, textes, images et éléments graphiques présents
+            sur ce site web sont la propriété exclusive d'Ilyes BEIRADE et sont
+            protégés par les lois sur la propriété intellectuelle. Toute
+            utilisation non autorisée de ces contenus est strictement interdite.
+          </p>
+        </section>
+
+        <section className="mt-6">
+          <h2 className="text-xl font-bold">Contact</h2>
+          <p>
+            Si vous avez des questions ou des préoccupations concernant nos
+            mentions légales, n'hésitez pas à nous contacter à l'adresse
+            i.beirade@gmail.com ou en appelant 07 78 90 74 44.
+          </p>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default MentionLegales;
