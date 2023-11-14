6c5cc9f64ed856078f01752c3bf82f50c3532fc4
y28lyn <i.beirade@gmail.com>
2023-11-14 17:07:54 +0100

    title updated

--- src/components/article/CardArticle.tsx
+++ src/components/article/CardArticle.tsx
@@ -118,7 +118,7 @@ export default function CardArticle() {
         "https://images.unsplash.com/photo-1619359059287-9d024d7081ef?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGlwYWQlMjBwcm8lMjB3YWxscGFwZXJ8ZW58MHx8MHx8fDA%3D",
       link: "#",
       linkUrl: "#",
-      titleModal: "Continuation d'SGRC",
+      titleModal: "Installation d'un GLPI",
       contexte: "",
       besoin: "",
       environnement: "",
