a4d4b269797a874b41f7d51a568bcff27fc37e82
y28lyn <i.beirade@gmail.com>
2024-01-11 18:37:19 +0100

    new article added in "veille"

--- src/pages/Veille.tsx
+++ src/pages/Veille.tsx
@@ -8,7 +8,7 @@ const Veille = () => {
       date: "12 Octobre, 2023",
       author: "SID SHUMAN",
       title: "Contrôleur Access PS5",
-      desc: "La manette Access pour la PS5 sortira le 6 décembre 2023 et permettra aux joueurs en situation de handicap de jouer plus confortablement et plus longtemps...",
+      desc: "La manette Access pour la PS5 sortira le 6 décembre 2023 et permettra aux joueurs en situation de handicap de jouer plus confortablement et plus longtemps.",
       imgUrl:
         "https://blog.playstation.com/tachyon/2023/10/95664514e9fff8f3455fc015bbab08ef0e101523-scaled.jpg?resize=1088%2C612&crop_strategy=smart&zoom=1.5",
       linkUrl:
@@ -19,7 +19,7 @@ const Veille = () => {
       date: "13 Novembre, 2023",
       author: "Yann",
       title: "WYES, des lunettes parlantes",
-      desc: "WYES est en réalité une paire de lunette couplée avec un module de connexion pour les personnes atteintes d'handicap afin de communiquer via les yeux...",
+      desc: "WYES est en réalité une paire de lunette couplée avec un module de connexion pour les personnes atteintes d'handicap afin de communiquer via les yeux.",
       imgUrl:
         "https://static.wixstatic.com/media/0be6df_b02c0ea8dc5446a78e060d40e83beb32~mv2.jpg/v1/crop/x_0,y_0,w_3584,h_1888/fill/w_725,h_381,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/10-28.jpg",
       linkUrl:
@@ -35,6 +35,16 @@ const Veille = () => {
         "https://images.unsplash.com/photo-1571330735066-03aaa9429d89?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       linkUrl:
         "https://www.francebleu.fr/infos/societe/orleans-lance-une-application-numerique-pour-rendre-la-ville-accessible-a-tous-1496977",
+    },
+    {
+      source: "Arte",
+      date: "6 Janvier, 2024",
+      author: "F. Rihouay",
+      title: "Contrôler son environnement par la pensée",
+      desc: "À Toronto, une technologie inédite permet à de jeunes enfants lourdement handicapés de jouer, peindre ou encore d’écouter de la musique comme n’importe quel autre enfant.",
+      imgUrl: "arte.png",
+      linkUrl:
+        "https://www.arte.tv/fr/videos/118414-000-A/handicap-controler-son-environnement-par-la-pensee/",
     },
   ];
 
