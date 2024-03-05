37aa29e0c0e37e850202bb7a19a6b0a9c6261a06
y28lyn <i.beirade@gmail.com>
2024-03-05 13:09:55 +0100

    style: :lipstick: Contact form's style updated

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -51,28 +51,97 @@ const Contact = () => {
   return (
     <>
       <main className="bg-[#000302] p-10 pt-20 flex flex-col md:flex-row justify-center gap-6">
-        <img
-          className="rounded-lg shadow-lg h-[10vh] md:h-auto w-full md:w-[50%] object-cover"
-          src="https://images.unsplash.com/photo-1707343848723-bd87dea7b118?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt=""
-        />
-        <section className="bg-[#fbfcf8] text-black p-8 rounded-lg space-y-6">
-          <h2 className="text-3xl font-bold text-center">
+        <div className="p-6 mr-2 bg-slate-800 rounded md:w-[60%] w-full">
+          <h1 className="text-4xl sm:text-5xl text-white font-extrabold tracking-tight">
             Prenez contact avec moi
-          </h2>
-          <p className="text-center text-lg">
-            Je serrais ravis de vous entendre !
+          </h1>
+          <p className="text-normal text-lg sm:text-2xl font-medium text-gray-400 mt-2">
+            Remplissez le formulaire pour entamer une conversation. Je serrais
+            ravis de vous entendre !
           </p>
+
+          <div className="flex items-center mt-8 text-gray-400">
+            <svg
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="1.5"
+              viewBox="0 0 24 24"
+              className="w-8 h-8 text-gray-500"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="1.5"
+                d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+              />
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="1.5"
+                d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+            </svg>
+            <div className="ml-4 text-md tracking-wide font-semibold w-40">
+              Lyon 69000, France
+            </div>
+          </div>
+
+          <div className="flex items-center mt-4 text-gray-400">
+            <svg
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="1.5"
+              viewBox="0 0 24 24"
+              className="w-8 h-8 text-gray-500"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="1.5"
+                d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+              />
+            </svg>
+            <div className="ml-4 text-md tracking-wide font-semibold w-40">
+              +33 7 78 90 74 44
+            </div>
+          </div>
+
+          <div className="flex items-center mt-2 text-gray-400">
+            <svg
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="1.5"
+              viewBox="0 0 24 24"
+              className="w-8 h-8 text-gray-500"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="1.5"
+                d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+              />
+            </svg>
+            <div className="ml-4 text-md tracking-wide font-semibold w-40">
+              i.beirade@gmail.com
+            </div>
+          </div>
+
           <form
             action="#"
             ref={form}
             onSubmit={sendEmail}
             method="POST"
-            className="space-y-4"
+            className="space-y-4 mt-8"
           >
             <div className="space-y-2">
               <div>
-                <label className="text-base font-medium text-gray-900">
+                <label className="text-base font-medium text-gray-200">
                   Nom et prénom
                 </label>
                 <div className="mt-2.5 relative">
@@ -87,7 +156,7 @@ const Contact = () => {
               </div>
 
               <div>
-                <label className="text-base font-medium text-gray-900">
+                <label className="text-base font-medium text-gray-200">
                   Email
                 </label>
                 <div className="mt-2.5 relative">
@@ -102,7 +171,7 @@ const Contact = () => {
               </div>
 
               <div>
-                <label className="text-base font-medium text-gray-900">
+                <label className="text-base font-medium text-gray-200">
                   Message
                 </label>
                 <div className="mt-2.5 relative">
@@ -115,10 +184,10 @@ const Contact = () => {
                 </div>
               </div>
 
-              <div>
+              <div className="flex justify-center md:justify-start">
                 <button
                   type="submit"
-                  className="mt-3 w-64 inline-flex items-center justify-center px-4 py-4 text-base font-semibold text-white bg-[#000302] rounded transition ease-in-out delay-150 md:hover:scale-105 duration-300"
+                  className="mt-3 w-64 inline-flex items-center justify-center px-4 py-4 text-base font-semibold text-[#000302] bg-white rounded transition ease-in-out delay-150 md:hover:scale-105 duration-300"
                 >
                   Envoyez votre message
                 </button>
@@ -133,7 +202,7 @@ const Contact = () => {
           {errorMessage && (
             <div className="mt-4 text-red-500">{errorMessage}</div>
           )}
-        </section>
+        </div>
       </main>
     </>
   );
