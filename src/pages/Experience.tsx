e7e1661ca8151e941360edfc9b4cd1e41fab3695
y28lyn <i.beirade@gmail.com>
2025-03-12 13:36:15 +0100

    refactor: :speech_balloon: text update to reduce size of the box in the experience's page

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -44,7 +44,7 @@ const Experience = () => {
         color: "bg-orange-300 dark:bg-orange-500",
       },
       description:
-        "En tant que volontaire en service civique à l’AFEV, j’accompagne des jeunes en difficulté scolaire en leur offrant un soutien personnalisé pour renforcer leur motivation et leur confiance en eux. J’interviens lors de séances de mentorat pour les aider dans leurs devoirs et développer des compétences transversales comme l’organisation et la prise de parole. Je participe également à des projets collectifs pour sensibiliser aux inégalités éducatives et promouvoir l’engagement citoyen.",
+        "En tant que volontaire en service civique à l’AFEV, j’accompagne des jeunes en difficulté scolaire en leur offrant un soutien personnalisé pour renforcer leur motivation et leur confiance en eux. J’interviens lors de séances de mentorat pour les aider dans leurs devoirs et développer des compétences transversales comme l’organisation et la prise de parole.",
       logoUrl: 
         "https://images.unsplash.com/photo-1606761568499-6d2451b23c66?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
