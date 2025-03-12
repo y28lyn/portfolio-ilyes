ab9a1386df5e19c7c9b2d05de857e336dfdace56
y28lyn <i.beirade@gmail.com>
2025-03-12 13:33:39 +0100

    feat: :bug: fixed a bug with experiences

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -1,82 +1,72 @@
-import React from "react";
+import ExperienceArticle from "../components/article/ExperienceArticle";
+import Banner from "../components/header/Banner";
 
-interface ExperienceItemProps {
-  position: string;
-  company: string;
-  timePeriod: {
-    year: string;
-    color: string;
-  };
-  description: string;
-  logoUrl: string;
-}
+const Experience = () => {
+  const experienceData = [
+    {
+      position: "Employé polyvalent",
+      company: "Cinéma Les Alizées",
+      timePeriod: { year: "Octobre 2021", color: "bg-rose-300 dark:bg-rose-500" },
+      description:
+        "Au cinéma de Bron, j'ai pu joué un rôle central durant mes études en accueillant les visiteurs, en assurant le contrôle du pass sanitaire, et en gérant l'ouverture/fermeture des salles. Cette expérience a renforcé ma polyvalence et ma volonté de contribuer à une atmosphère conviviale, tout en participant au succès du cinéma.",
+      logoUrl:
+        "https://images.unsplash.com/photo-1536440136628-849c177e76a1?q=80&w=1925&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      position: "Agent de nettoiement",
+      company: "Grand Lyon",
+      timePeriod: {
+        year: "Juillet 2024",
+        color: "bg-emerald-300 dark:bg-emerald-500",
+      },
+      description:
+        "J'ai occupé le poste d'agent de nettoiement saisonnier au sein d'une équipe. Mon rôle consistait à assurer la propreté et l'entretien des espaces publics, contribuant ainsi à créer un environnement agréable pour les résidents et les visiteurs. J'ai acquis une expérience dans les tâches de nettoyage et d'entretien des espaces extérieurs.",
+      logoUrl:
+        "https://images.unsplash.com/photo-1610349100246-9535097a884f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      position: "Développeur web",
+      company: "INOLIB",
+      timePeriod: {
+        year: "Janvier 2024 - Février 2024",
+        color: "bg-blue-300 dark:bg-blue-500",
+      },
+      description:
+        "En tant que stagiaire chez Inolib, j'ai eu le privilège de contribuer au développement de diverse site web de l'entreprise, en mettant l'accent sur l'accessibilité et l'expérience utilisateur. Pendant cette expérience transformatrice, j'ai travaillé en étroite collaboration avec une équipe dynamique de professionnels.",
+      logoUrl:
+        "https://images.unsplash.com/photo-1498758536662-35b82cd15e29?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGJsdWUlMjBjb21wdXRlcnxlbnwwfHwwfHx8MA%3D%3D",
+    },
+    {
+      position: "Volontaire en service civique",
+      company: "AFEV",
+      timePeriod: {
+        year: "Octobre 2024 - Juin 2025",
+        color: "bg-orange-300 dark:bg-orange-500",
+      },
+      description:
+        "En tant que volontaire en service civique à l’AFEV, j’accompagne des jeunes en difficulté scolaire en leur offrant un soutien personnalisé pour renforcer leur motivation et leur confiance en eux. J’interviens lors de séances de mentorat pour les aider dans leurs devoirs et développer des compétences transversales comme l’organisation et la prise de parole. Je participe également à des projets collectifs pour sensibiliser aux inégalités éducatives et promouvoir l’engagement citoyen.",
+      logoUrl: 
+        "https://images.unsplash.com/photo-1606761568499-6d2451b23c66?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+  ];
 
-interface ExperienceArticleProps {
-  experiences: ExperienceItemProps[];
-}
-
-const ExperienceArticle: React.FC<ExperienceArticleProps> = ({
-  experiences,
-}) => {
   return (
-    <section className="flex justify-center w-full">
-      <div className="container px-0 md:px-6">
-        <div className="flex flex-col md:flex-row gap-6">
-          {experiences.map((experience, index) => (
-            <ExperienceItem
-              key={index}
-              {...experience}
-              invert={index % 2 === 1}
-            />
-          ))}
-        </div>
-      </div>
-    </section>
+    <>
+      <header>
+        <Banner
+          title="Mes expériences professionelles"
+          subtitle="Les moments clés de ma carrière, de débuts prometteurs à des défis professionnels relevés avec succès."
+          backgroundImageUrl="https://images.unsplash.com/photo-1569087306794-db9947393580?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          button1Label="Mes projets"
+          button1Link="/situation"
+          button2Label="Lire plus"
+        />
+      </header>
+      <main id="main" className="p-10 bg-white dark:bg-[#000302]">
+        <ExperienceArticle experiences={experienceData} />
+      </main>
+    </>
   );
 };
 
-const ExperienceItem: React.FC<ExperienceItemProps & { invert?: boolean }> = ({
-  position,
-  company,
-  timePeriod,
-  description,
-  logoUrl,
-  invert = false,
-}) => {
-  const bgClass = invert
-    ? "bg-blue-400 dark:bg-gray-800"
-    : "bg-blue-500 dark:bg-gray-900";
-  return (
-    <div
-      className={`p-6 w-full md:w-1/3 h-auto shadow-lg rounded ${bgClass} text-white`}
-    >
-      <div className="items-center justify-between text-base">
-        <div className="flex flex-row items-center">
-          <img
-            alt=""
-            className="w-20 h-20 mb-2 rounded-full"
-            src={logoUrl}
-            style={{
-              aspectRatio: "1 / 1",
-              objectFit: "cover",
-            }}
-          />
-          <div className="ml-6">
-            <h3 className="text-sm font-semibold">{position}</h3>
-            <p className="text-white">{company}</p>
-          </div>
-        </div>
-        <div
-          className={`text-[10px] md:text-sm md:text-center py-0 md:py-1 px-2 rounded mb-2 mt-2 ${timePeriod.color}`}
-        >
-          {timePeriod.year}
-        </div>
-      </div>
-      <div className="mt-2 text-gray-200">
-        <p>{description}</p>
-      </div>
-    </div>
-  );
-};
-
-export default ExperienceArticle;
+export default Experience;
